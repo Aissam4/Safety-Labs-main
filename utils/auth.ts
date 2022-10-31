8e9422ca9933c991569b65c8decbac13f8bd6ff1
Rachid oudouch <rashidoudouch@gmail.com>
2022-10-31 17:31:56 +0100

    protect auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -4,33 +4,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function checkAuth(req: any, res: any, next: any) {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = await verify_token(token);
+    try {
+        const token = req.headers.authorization?.split(' ')[1] || '';
+        const decoded = await verify_token(token);
 
-    if (decoded[0]) {
-        res.status(401).json({
-            message: 'Auth failed'
-        })
-    }
-    else {
-        await prisma.user.findUnique({
-            where: {
-                id: Number(decoded[1]?.sub)
-            }
-        }).then((data) => {
-            if (data) {
-                req.user = data;
-                next();
-            } else {
-                res.status(401).json({
-                    message: 'Auth failed'
-                })
-            }
-        }).catch((err) => {
+        if (decoded[0]) {
             res.status(401).json({
-                message: 'Auth failed',
-                err
+                message: 'Auth failed'
             })
+        }
+        else {
+            await prisma.user.findUnique({
+                where: {
+                    id: Number(decoded[1]?.sub)
+                }
+            }).then((data) => {
+                if (data) {
+                    req.user = data;
+                    next();
+                } else {
+                    res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                }
+            }).catch((err) => {
+                res.status(401).json({
+                    message: 'Auth failed',
+                    err
+                })
+            })
+        }
+    } catch (err) {
+        res.status(401).json({
+            message: 'Auth failed',
+            err
         })
     }
 }
